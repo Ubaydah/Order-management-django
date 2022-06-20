c0048304b9a903a3d10383161b7a4ee37fc53f68
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-06-20 11:10:59 +0100

    changes

--- orderApp/serializers.py
+++ orderApp/serializers.py
@@ -1,13 +1,10 @@
 from rest_framework import serializers
-
 from .models import Order, Shipment
-
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ["id", "name", "quantity", "weight", "price", "created_at"]
-
 
 class ShipmentSerializer(serializers.ModelSerializer):
     order = OrderSerializer(read_only=True)
@@ -25,15 +22,11 @@ class ShipmentSerializer(serializers.ModelSerializer):
             "shipment_date",
             "status",
         ]
-
     def create(self, validated_data):
         try:
-
             order = Order.objects.get(id=validated_data["order_id"])
-
             shipment = Shipment.objects.create(order=order, **validated_data)
 
             return shipment
         except Order.DoesNotExist:
-
             return serializers.ValidationError("This order doesn't exist")
